02bcedfca2b7b6da06873c0adaadaca56472e422
GiacomoP99 <giacomo090909@gmail.com>
2022-06-27 13:09:25 +0200

    LM4
    pre lunch

--- ToDoAngular/src/app/app.module.ts
+++ ToDoAngular/src/app/app.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CourseComponent } from './course/course.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, CourseComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [CourseComponent],
 })
-export class AppModule { }
+export class AppModule {}
